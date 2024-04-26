b2f963240fbf4b3b7ea521de304723286183e358
Donnie Goodson <49205731+donnie-msft@users.noreply.github.com>
2024-04-26 11:34:39 -0700

    ***NO_CI*** WIP

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Models/DetailedPackageMetadata.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Models/DetailedPackageMetadata.cs
@@ -24,6 +24,7 @@ namespace NuGet.PackageManagement.UI
             Description = serverData.Description;
             Authors = serverData.Authors;
             Owners = serverData.Owners;
+            KnownOwners = serverData.KnownOwners;
             IconUrl = serverData.IconUrl;
             LicenseUrl = serverData.LicenseUrl;
             ProjectUrl = serverData.ProjectUrl;
@@ -82,7 +83,7 @@ namespace NuGet.PackageManagement.UI
         public string Authors { get; set; }
 
         public string Owners { get; set; }
-
+        public IReadOnlyList<KnownOwner> KnownOwners { get; private set; }
         public Uri IconUrl { get; set; }
 
         public Uri LicenseUrl { get; set; }
